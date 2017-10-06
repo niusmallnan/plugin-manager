ea8e0e8a2be43a15a7e6792ca99724fee9f93ce9
niusmallnan <niusmallnan@gmail.com>
2017-10-06 18:08:26 +0800

    Dynamic value injection support in CNI config
    Adding this for l2-flat networking. https://github.com/rancher/rancher/issues/8673

--- utils/utils.go
+++ utils/utils.go
@@ -3,40 +3,86 @@ package utils
 import (
 	"strings"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/pkg/errors"
 	"github.com/rancher/go-rancher-metadata/metadata"
+	"github.com/vishvananda/netlink"
 )
 
 const (
-	hostLabelKeyword = "__host_label__"
+	hostLabelKeyword     = "__host_label__"
+	hostInterfaceKeyword = "__host_interface__"
 )
 
 // UpdateCNIConfigByKeywords takes in the given CNI config, replaces the rancher
 // specific keywords with the appropriate values.
 func UpdateCNIConfigByKeywords(config interface{}, host metadata.Host) interface{} {
-	props, isMap := config.(map[string]interface{})
-	if !isMap {
-		return config
-	}
-
-	for aKey, aValue := range props {
-		if v, isString := aValue.(string); isString {
-			if strings.HasPrefix(v, hostLabelKeyword) {
-				props[aKey] = ""
-				splits := strings.SplitN(v, ":", 2)
-				if len(splits) > 1 {
-					label := strings.TrimSpace(splits[1])
-					labelValue := host.Labels[label]
-					if labelValue != "" {
-						props[aKey] = labelValue
+	switch props := config.(type) {
+	case []interface{}:
+		for i, v := range props {
+			tempValue := UpdateCNIConfigByKeywords(v, host)
+			props[i] = tempValue
+		}
+		return props
+	case map[string]interface{}:
+		for aKey, aValue := range props {
+			if v, isString := aValue.(string); isString {
+				if strings.HasPrefix(v, hostLabelKeyword) {
+					props[aKey] = ""
+					splits := strings.SplitN(v, ":", 2)
+					if len(splits) > 1 {
+						label := strings.TrimSpace(splits[1])
+						labelValue := host.Labels[label]
+						if labelValue != "" {
+							props[aKey] = labelValue
+						}
 					}
 				}
+				if strings.HasPrefix(v, hostInterfaceKeyword) {
+					props[aKey] = ""
+					splits := strings.SplitN(v, ":", 2)
+					if len(splits) > 1 {
+						iface := strings.TrimSpace(splits[1])
+						ipstr, err := getIPFromIface(iface, 0)
+						if err != nil {
+							logrus.Errorf("Got error from UpdateCNIConfigByKeywords: %v", err)
+							break
+						}
+						props[aKey] = ipstr
+					}
+				}
+			} else {
+				tempValue := UpdateCNIConfigByKeywords(aValue, host)
+				props[aKey] = tempValue
 			}
-		} else {
-			props[aKey] = UpdateCNIConfigByKeywords(aValue, host)
 		}
+		return props
+	default:
+		return config
+	}
+}
+
+func getIPFromIface(iface string, index int) (string, error) {
+	ipstr := ""
+	l, err := netlink.LinkByName(iface)
+	if err != nil {
+		return ipstr, errors.Wrap(err, "Failed to get bridge link")
 	}
 
-	return props
+	addrs, err := netlink.AddrList(l, netlink.FAMILY_V4)
+	if err != nil {
+		return ipstr, errors.Wrap(err, "Failed to get bridge address")
+	}
+
+	if len(addrs) > index {
+		ipstr = strings.Split(addrs[index].IPNet.String(), "/")[0]
+	}
+
+	if ipstr == "" {
+		return ipstr, errors.Errorf("Get no ip address from %s", iface)
+	}
+
+	return ipstr, nil
 }
 
 // GetBridgeInfo is used to figure out the bridge information from the
